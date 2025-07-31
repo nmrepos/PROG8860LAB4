481fb05d8f9aa7f8debeba6606dca4c6162f77af
Nidhun Murali <nidhunofficial@gmail.com>
2025-07-31 23:27:27 +0000

    Updated

---
+++ test_app.py
@@ -0,0 +1,8 @@
+import app
+import azure.functions as func
+
+def test_main():
+    req = func.HttpRequest(method='GET', url='/api/hello', body=None)
+    resp = app.main(req)
+    assert resp.status_code == 200
+    assert resp.get_body().decode() == "Hello, world!"
